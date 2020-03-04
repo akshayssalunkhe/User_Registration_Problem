1b157f2a13a90aa998f6c0de9941d8020eb57660
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-04 17:39:43 +0530

    ADDED:Method To Check Last Name

--- src/main/java/UserRegistrationProblem.java
+++ src/main/java/UserRegistrationProblem.java
@@ -2,15 +2,23 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
     //PATTERN OF FIRST NAME
-    public String firstNamePattern ="^[A-Z]{1}[a-z]{2,}$";
+    public String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+    //PATTERN OF LAST NAME
+    public String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
 
     //MAIN METHOD
     public static void main(String[] args) {
         //WELCOME MESSAGE
         System.out.println("Welcome To User Registration Problem");
     }
+
     //METHOD TO VALIDATE FIRST NAME
-    public boolean validateFirstName(String firstname) {
-        return (Pattern.matches(firstNamePattern,firstname));
+    public boolean validateFirstName(String firstName) {
+        return (Pattern.matches(firstNamePattern, firstName));
+    }
+
+    //METHOD TO VALIDATE LAST NAME
+    public boolean validateLastName(String lastName) {
+        return (Pattern.matches(lastNamePattern, lastName));
     }
 }
--- src/test/java/UserRegistrationProblemTest.java
+++ src/test/java/UserRegistrationProblemTest.java
@@ -3,19 +3,34 @@ import org.junit.Test;
 
 public class UserRegistrationProblemTest {
 
+    //CREATING COMMON OBJECT FOR ALL TEST CASES
+    UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
+
     //TEST CASE FOR VALID FIRST NAME PATTERN
     @Test
     public void giveFirstName_WhenValid_ThenReturn() {
-        UserRegistrationProblem userRegistrationProblem=new UserRegistrationProblem();
         boolean firstName = userRegistrationProblem.validateFirstName("Akshay");
         Assert.assertTrue(firstName);
     }
 
     //TEST CASE FOR INVALID FIRST NAME PATTERN
     @Test
-    public void givenFirstName_WhenValid_ThenReturn() {
-        UserRegistrationProblem userRegistrationProblem=new UserRegistrationProblem();
+    public void givenFirstName_WhenInvalid_ThenReturn() {
         boolean firstName = userRegistrationProblem.validateFirstName("akshay");
         Assert.assertFalse(firstName);
+    }
+
+    //TEST CASE FOR VALID LAST NAME PATTERN
+    @Test
+    public void giveLastName_WhenValid_ThenReturn() {
+        boolean lastName = userRegistrationProblem.validateLastName("Salunkhe");
+        Assert.assertTrue(lastName);
+    }
+
+    //TEST CASE FOR INVALID LAST NAME PATTERN
+    @Test
+    public void giveLastName_WhenInvalid_ThenReturn() {
+        boolean lastName = userRegistrationProblem.validateLastName("salunkhe");
+        Assert.assertFalse(lastName);
     }
 }
