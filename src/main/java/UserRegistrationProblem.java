57570bf96cc22b2fcab183d6ee517b612e638db3
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-05 10:50:58 +0530

    ADDED:Method To Validate Email Ids

--- src/main/java/UserRegistrationProblem.java
+++ src/main/java/UserRegistrationProblem.java
@@ -5,6 +5,8 @@ public class UserRegistrationProblem {
     public String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
     //PATTERN OF LAST NAME
     public String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+    //PATTERN OF EMAIL ID
+    public String emailIdPattern = "^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$";
 
     //MAIN METHOD
     public static void main(String[] args) {
@@ -20,5 +22,9 @@ public class UserRegistrationProblem {
     //METHOD TO VALIDATE LAST NAME
     public boolean validateLastName(String lastName) {
         return (Pattern.matches(lastNamePattern, lastName));
+    }
+    //METHOD TO VALIDATE EMAIL IDS
+    public boolean validateEmailId(String email) {
+        return (Pattern.matches(emailIdPattern,email));
     }
 }
--- src/test/java/UserRegistrationProblemTest.java
+++ src/test/java/UserRegistrationProblemTest.java
@@ -3,6 +3,32 @@ import org.junit.Test;
 
 public class UserRegistrationProblemTest {
 
+    //STRING ARRAY OF VALID EMAIL IDS
+    String [] validEmailIds={"abc@yahoo.com",
+                            "abc-100@yahoo.com",
+                            "abc.100@yahoo.com",
+                            "abc111@abc.com",
+                            "abc-100@abc.net",
+                            "abc.100@abc.com.au",
+                            "abc@1.com",
+                            "abc@gmail.com.com",
+                            "abc+100@gmail.com"};
+
+    //STRING ARRAY OF INVALID EMAIL IDS
+    String [] invalidEmailIds={"abc",
+                               "abc@.com.my",
+                               "abc123@gmail.a",
+                               "abc123@.com",
+                               "abc123@.com.com",
+                               ".abc@abc.com",
+                               "abc()*@gmail.com",
+                               "abc@%*.com",
+                               "abc..2002@gmail.com",
+                               "abc.@gmail.com",
+                               "abc@abc@gmail.com",
+                               "abc@gmail.com.1a",
+                               "abc@gmail.com.aa.au"};
+
     //CREATING COMMON OBJECT FOR ALL TEST CASES
     UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
 
@@ -33,4 +59,25 @@ public class UserRegistrationProblemTest {
         boolean lastName = userRegistrationProblem.validateLastName("salunkhe");
         Assert.assertFalse(lastName);
     }
+    //TEST CASE FOR VALID EMAIL ID
+    @Test
+    public void giveEmailId_WhenValid_ThenReturn () {
+        for ( int index=0 ;index<validEmailIds.length; index++ )
+        {
+            boolean emailId = userRegistrationProblem.validateEmailId(validEmailIds[index]);
+            Assert.assertTrue(emailId);
+        }
+    }
+
+    //TEST CASE FOR INVALID EMAIL IDS
+    @Test
+    public void giveEmailId_WhenInvalid_ThenReturn () {
+        for ( int index=0 ;index<invalidEmailIds.length; index++ )
+        {
+            boolean invalidEmailId = userRegistrationProblem.validateEmailId(invalidEmailIds[index]);
+            Assert.assertFalse(invalidEmailId);
+            Assert.assertFalse(invalidEmailId);
+        }
+    }
+
 }
